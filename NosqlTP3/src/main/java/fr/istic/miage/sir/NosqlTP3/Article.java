b76c76fb9fb6ce2ea76392b30fed396e19a0f308
EL MAHJOUB Mohamed-Abdrahmane <mahjoubwel@gmail.com>
2021-10-28 15:01:04 +0200

    mongodb

---
+++ NosqlTP3/src/main/java/fr/istic/miage/sir/NosqlTP3/Article.java
@@ -0,0 +1,46 @@
+package fr.istic.miage.sir.NosqlTP3;
+
+import java.util.List;
+
+import org.bson.types.ObjectId;
+import org.mongodb.morphia.annotations.Entity;
+import org.mongodb.morphia.annotations.Id;
+
+@Entity
+public class Article {
+
+    @Id
+    private ObjectId id;
+    private String name;
+    private int stars;
+    
+    private List<Person> persons;
+    
+	public ObjectId getId() {
+		return id;
+	}
+	public void setId(ObjectId id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getStars() {
+		return stars;
+	}
+	public void setStars(int stars) {
+		this.stars = stars;
+	}
+	public List<Person> getPersons() {
+		return persons;
+	}
+	public void setPersons(List<Person> persons) {
+		this.persons = persons;
+	}
+    
+    
+    
+}
---
+++ NosqlTP3/src/main/java/fr/istic/miage/sir/NosqlTP3/Person.java
@@ -0,0 +1,39 @@
+package fr.istic.miage.sir.NosqlTP3;
+
+import java.util.List;
+
+import org.bson.types.ObjectId;
+import org.mongodb.morphia.annotations.Entity;
+import org.mongodb.morphia.annotations.Id;
+
+@Entity
+public class Person {
+
+    @Id
+    private ObjectId id;
+    private String name;
+    
+    private List<Address> address;
+    
+    
+	public ObjectId getId() {
+		return id;
+	}
+	public void setId(ObjectId id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public List<Address> getAdress() {
+		return address;
+	}
+	public void setAddress(Address adress) {
+		this.address.add(adress) ;
+	}
+    
+    
+}
